77b2c73bc9a219461c7aba5d4f9cd9d29d81400d
Brian <brian.bogos@yahoo.com>
2018-09-26 20:25:30 -0400

    Iniitial commit

---
+++ DatingApp.API/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // this is where we add services available to be injected into our application
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // needed to nuget this in via microsoft.entityframworkcore.sqlite ( version 2.1)
+            services.AddDbContext<DataContext>( x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")) );
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(); // cross origin resource sharing
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // app.UseHsts();
+            }
+
+            // app.UseHttpsRedirection();
+            // required before .usemvc. Weak for training purposes
+            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader() ); 
+            app.UseMvc();
+        }
+    }
+}
