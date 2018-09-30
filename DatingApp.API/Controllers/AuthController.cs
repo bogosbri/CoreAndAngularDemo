4303b2124316823e43c0fd70ee8f3917d5018700
Brian <brian.bogos@yahoo.com>
2018-09-29 20:11:11 -0400

    End of section 3

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")] // api/auth
+    [ApiController] // infers where data is coming from without [FromBody]
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            this._repo = repo;
+            this._config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register( UserForRegisterDto userForRegisterDto)
+        {
+            // this isn't needed when we are using the [ApiController] attribute
+            // if( !ModelState.IsValid )
+            // {
+            //     return BadRequest(ModelState);
+            // }
+            
+            // first we want to validate the request
+            var username =  userForRegisterDto.UserName.ToLower();
+            if( await _repo.UserExists(username) )
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User
+            {
+                UserName = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201); // for now
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login( UserForLoginDto userforLoginDto)
+        {
+            if (userforLoginDto == null)
+            {
+                throw new System.ArgumentNullException(nameof(userforLoginDto));
+            }
+
+            var userFromRepo = await _repo.Login(userforLoginDto.Username.ToLower(), userforLoginDto.Password);
+            if( userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString() ),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName )
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // contains claims, expiratiopn and credentials
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok( new {
+                token= tokenHandler.WriteToken(token)
+            });
+
+
+        }
+
+
+
+
+
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API
 {
@@ -32,6 +35,21 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>( x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")) );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors(); // cross origin resource sharing
+
+            // transient is for short term items. addscoped is kind of like singleton per http request - so calls within the same httprequest are single but a new request will create 
+            // a new one
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer( options => {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +67,7 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
             // required before .usemvc. Weak for training purposes
             app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader() ); 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
